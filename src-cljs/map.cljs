d355c7e9f93b943b7aa0dc71a602670ef182c523
Logit <milos.miletic@logit-solutions.com>
2016-11-08 21:23:04 +0100

    Added initial code for map representation.

--- src-cljs/map.cljs
+++ src-cljs/map.cljs
@@ -1,3 +1,34 @@
-(ns cljs.map)
+(ns cljs.map
+    (:require ol.Map
+            ol.Collection
+            ol.layer.Tile
+            ol.View
+            ol.proj
+            ol.extent
+            ol.source.OSM
 
-;; Shomy work you magic here!
+            ol.style.Style
+            ol.style.Fill
+            ol.style.Stroke
+            ol.style.Circle
+            ol.interaction.Draw
+            ol.interaction.Pointer
+            ol.interaction.Select
+
+            ol.interaction.Translate
+            ol.events.condition
+            ol.geom.Polygon
+            ol.geom.Point))
+
+(set! (.-onload js/window)
+  (fn []
+    (let [source (ol.source.OSM. #js {:layer "sat"})
+          raster (ol.layer.Tile. #js {:source source})
+          view (ol.View. #js {:center (ol.proj.fromLonLat #js [20.4489 44.7866])
+                            :zoom 11
+                            :maxZoom 18})
+           ]
+      (map (ol.Map. #js {:layers #js [raster ]
+                          :target "map"
+                          :view   view})))))
+
