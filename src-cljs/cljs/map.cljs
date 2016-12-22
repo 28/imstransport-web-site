519b0be09f744232255332239bd7913bcb150dee
Milos <shomy4@gmail.com>
2016-12-22 21:51:35 +0100

    Modified handler for successfull api response

--- src-cljs/cljs/map.cljs
+++ src-cljs/cljs/map.cljs
@@ -11,14 +11,17 @@
             ol.interaction.Draw
             ol.geom.Polygon
             ol.geom.Point
-            [ajax.core :refer [GET POST]]
+            [ajax.core :refer [json-response-format json-request-format ajax-request]]
             [cljs.reader :as reader]))
 
 (enable-console-print!)
 
-(defn get-price-information [response]
-  (let [price (aget  (.-arr response) 1)
+(defn get-price-information [[ok response]]
+
+  (let [response-obj (clj->js response)
+        price (aget response-obj "price")
         message (str "Cena je: " price " dinara.")]
+        (js/console.log response-obj)
   (js/alert  message)))
 
 (defn draw-end-handler [e]
@@ -26,13 +29,16 @@
   (let [coords (.getCoordinates (.getGeometry (.-feature e)))
         first-coord (aget js/coords 0)
         end-coord (aget js/coords 1)]
-    (POST "/api"
-    {:params {:origin {:lat (aget js/first-coord 1) :long (aget js/first-coord 0)}
-                        :dest {:lat (aget js/end-coord 1) :long (aget js/end-coord 0)}
-                        :in-belgrade true}
-     :format :json
-     :response-format :json
-     :handler get-price-information})))
+        (ajax-request
+            {:uri "/api"
+             :method :post
+             :params {:origin {:lat (aget js/first-coord 1) :long (aget js/first-coord 0)}
+                                 :dest {:lat (aget js/end-coord 1) :long (aget js/end-coord 0)}
+                                 :in-belgrade true}
+             :handler get-price-information
+             :format (json-request-format)
+             :response-format (json-response-format {:keywords? true})
+             :keywords true})))
 
 (defn create-transport-map []
   (let [rasterSource (ol.source.OSM. #js {:layer "sat"})
