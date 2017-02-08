27bd9b9071a9d78f4b34cc17d1fc8d36e9bf7060
Dejan Josifovic <Rajoduo@yahoo.com>
2017-02-08 22:01:33 +0100

    Refactor map.cljs to fix compiler warnings about shadowed locals; reformat :)

--- src-cljs/cljs/map.cljs
+++ src-cljs/cljs/map.cljs
@@ -21,38 +21,38 @@
         price (aget response-obj "price")
         message (str "Cena je: " price " dinara.")]
     (js/console.log response-obj)
-    (js/alert  message)))
+    (js/alert message)))
 
 (defn draw-end-handler [e]
   (let [coords (.getCoordinates (.getGeometry (.-feature e)))
-        first-coord (aget js/coords 0)
-        end-coord (aget js/coords 1)]
+        first-coord (aget coords 0)
+        end-coord (aget coords 1)]
     (ajax-request
-     {:uri "/api"
-      :method :post
-      :params {:origin {:lat (aget js/first-coord 1) :long (aget js/first-coord 0)}
-               :dest {:lat (aget js/end-coord 1) :long (aget js/end-coord 0)}}
-      :handler get-price-information
-      :response-format :json
-      :format :json
-      :keywords? true})))
+      {:uri             "/api"
+       :method          :post
+       :params          {:origin {:lat (aget first-coord 1) :long (aget first-coord 0)}
+                         :dest   {:lat (aget end-coord 1) :long (aget end-coord 0)}}
+       :handler         get-price-information
+       :response-format :json
+       :format          :json
+       :keywords?       true})))
 
 (defn create-transport-map []
   (let [rasterSource (ol.source.OSM. #js {:layer "sat"})
         rasterLayer (ol.layer.Tile. #js {:source rasterSource})
-        view (ol.View. #js {:center  #js [20.4489 44.7866]
-                            :zoom 11
-                            :extent #js [18.5 40.4 22.7 47.5]
-                            :minZoom 7
-                            :maxZoom 19
+        view (ol.View. #js {:center     #js [20.4489 44.7866]
+                            :zoom       11
+                            :extent     #js [18.5 40.4 22.7 47.5]
+                            :minZoom    7
+                            :maxZoom    19
                             :projection "EPSG:4326"})
         vectorSource (ol.source.Vector.)
-        vectorLayer  (ol.layer.Vector. #js {:source vectorSource})
+        vectorLayer (ol.layer.Vector. #js {:source vectorSource})
         drawInteraction (ol.interaction.Draw. #js {:source vectorSource
-                                                   :type "LineString"})
+                                                   :type   "LineString"})
         map (ol.Map. #js {:layers #js [rasterLayer, vectorLayer]
                           :target "map"
-                          :view view})]
+                          :view   view})]
     (.addInteraction map drawInteraction)
     (.on drawInteraction "drawend" draw-end-handler)))
 
